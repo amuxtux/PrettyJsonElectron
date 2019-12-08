27193c80f628b5be3d2e262a30a84489caddbb01
Amit Singhai <amitsinghai.jain@gmail.com>
2019-12-08 22:27:45 +0530

    first version

---
+++ PrettyJson-darwin-x64/PrettyJson.app/Contents/Resources/app/app.js
@@ -0,0 +1,55 @@
+const {app, BrowserWindow, Menu} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let window = null
+
+// Wait until the app is ready
+app.once('ready', () => {
+  // Create a new window
+  window = new BrowserWindow({
+    // Set the initial width to 800px
+    width: 800,
+    // Set the initial height to 600px
+    height: 600,
+    // Set the default background color of the window to match the CSS
+    // background color of the page, this prevents any white flickering
+    backgroundColor: "#D6D8DC",
+    // Don't show the window until it's ready, this prevents any white flickering
+    show: false
+  })
+
+  // Load a URL in the window to the local index.html path
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Show window when page is ready
+  window.once('ready-to-show', () => {
+    window.show()
+  })
+
+
+var template = [{
+  label: "Application",
+  submenu: [
+      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+      { type: "separator" },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+  ]}, {
+  label: "Edit",
+  submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+  ]}
+];
+
+Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+})
